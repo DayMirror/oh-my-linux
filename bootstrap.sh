a7acac48903cc8ecba7980ce20bcd5b0b4e90b1b
Damir <damir.imaev@athlo.app>
2023-08-13 18:13:12 +0100

    Fixes & some additions.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,10 @@
 sh ./bootstrap/setup-zsh.sh
 sh ./bootstrap/install-git.sh
 sh ./bootstrap/install-ansible.sh
+
+sudo ansible-galaxy install -r requirements.yml
+sudo ansible-playbook oh-my-linux.yml
+
 sh ./bootstrap/install-nerd-fonts.sh
 sh ./bootstrap/install-nvm.sh
 sh ./bootstrap/install-sdkman.sh
@@ -10,7 +14,4 @@ sh ./bootstrap/install-pyenv.sh
 sh ./bootstrap/install-docker.sh
 sh ./bootstrap/install-elasticsearch.sh
 sh ./bootstrap/install-openlens.sh
-
-ansible-galaxy install -r requirements.yml
-ansible-playbook oh-my-linux.yml
-
+sh ./bootstrap/install-mongo.sh
