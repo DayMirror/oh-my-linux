4baec29042e369ab03c3bf47851382eb1834aae8
Damir <damir.imaev@athlo.app>
2023-08-13 20:26:21 +0100

    Prefer installation via sdkman or nvm

--- bootstrap/install-nvm.sh
+++ bootstrap/install-nvm.sh
@@ -3,3 +3,9 @@
 # node version manager
 sudo apt-get install wget
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+
+# make sure nvm is available
+source ~/.bashrc
+
+# install latest node
+nvm install --lts
--- bootstrap/install-sdkman.sh
+++ bootstrap/install-sdkman.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-# java version manager
+# install sdkman
 sudo apt-get install wget
 wget -qO- https://get.sdkman.io | bash
 
+# make sure sdk command available
+source ~/.bashrc
+
+# install latest versions of some tools
+sdk install java
+sdk install maven
+sdk install gradle
+sdk install groovy
+sdk install kotlin
